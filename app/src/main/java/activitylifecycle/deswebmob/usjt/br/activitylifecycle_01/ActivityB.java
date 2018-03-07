5663e49d8577c9eca339a72becfe4125726dc21b
arqdsis <arqdsis@mocli10205.animaedu.intranet>
2018-03-07 18:08:55 -0300

    Initial commit

---
+++ app/src/main/java/activitylifecycle/deswebmob/usjt/br/activitylifecycle_01/ActivityB.java
@@ -0,0 +1,13 @@
+package activitylifecycle.deswebmob.usjt.br.activitylifecycle_01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
